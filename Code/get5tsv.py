74f393f65c023f698ca662f8ad9ff8b71650b859
renyanwu <349426266@qq.com>
2019-12-10 21:33:24 -0700

    push

--- Code/get5tsv.py
+++ Code/get5tsv.py
@@ -3,9 +3,15 @@ import time
 
 with open('../data/questions.json', 'r') as f:
     questions = eval(f.read())['items']
-questions = pd.DataFrame(questions)[['owner', 'question_id']].dropna()
+questions = pd.DataFrame(questions)[['owner', 'question_id', 'score', 'creation_date']].dropna()
 questions['owner'] = questions['owner'].map(lambda x: x.get('user_id'))
 questions = questions.dropna().reset_index(drop=True)
+
+current = time.time()
+interval = [current - t1 for t1 in questions['creation_date']]
+upvotebytime = [ questions['score'][i] * 3600 * 24 / (current - questions['creation_date'][i]) for i in range(len(questions))]
+questions['q_upvotebytime'] = upvotebytime
+questions = questions[['owner', 'question_id', 'q_upvotebytime']]
 
 
 with open('../data/answers.json', 'r') as f:
@@ -14,21 +20,16 @@ ans = pd.DataFrame(ans)[['owner', 'question_id', 'score', 'creation_date']].drop
 ans['owner'] = ans['owner'].map(lambda x: x.get('user_id'))
 ans = ans.dropna().reset_index(drop=True)
 
-current = time.time()
 interval = [current - t1 for t1 in ans['creation_date']]
 upvotebytime = [ ans['score'][i] * 3600 * 24 / (current - ans['creation_date'][i]) for i in range(len(ans))]
-ans['upvotebytime'] = upvotebytime
-ans = ans[['owner', 'question_id', 'upvotebytime']]
+ans['a_upvotebytime'] = upvotebytime
+ans = ans[['owner', 'question_id', 'a_upvotebytime']]
 
-questions.columns = ['asker', 'question_id']
-ans.columns = ['answerer', 'question_id', 'upvotebytime']
-
-print(questions.head())
-print(ans.head())
+questions.columns = ['asker', 'question_id', 'q_upvotebytime']
+ans.columns = ['answerer', 'question_id', 'a_upvotebytime']
 
 df = pd.merge(questions, ans, how = 'left',on='question_id')
 df = df.dropna().reset_index(drop=True)
-print(df.head())
 
 
 #ARN
@@ -37,50 +38,60 @@ arn.columns = ['from', 'to']
 arn = arn.drop_duplicates()
 arn.to_csv('../data/1-ARN.tsv', index=False, sep = '\t')
 
-
 #ABAN
 best_ans = {}
 for i, row in df.iterrows():
-    if row['asker'] not in best_ans or row['upvotebytime'] > best_ans[row['asker']][1]:
-        best_ans[row['asker']] = [row['answerer'], row['upvotebytime']]
+    if row['asker'] not in best_ans or row['a_upvotebytime'] > best_ans[row['asker']][1]:
+        best_ans[row['asker']] = [row['answerer'], row['a_upvotebytime']]
 
 aban = {'from': list(best_ans.keys()), 'to': [x[0] for x in best_ans.values()]}
 aban = pd.DataFrame(aban)
 aban.to_csv('../data/2-ABAN.tsv', index=False, sep = '\t')
 
-#CBEN, VBEN, VBEN2
+#CBEN
 qs = set(df['question_id'])
 cben = {'from': [], 'to':[]}
-vben = {'from': [], 'to':[], 'weight': []}
-vben2 = {'from': [], 'to':[], 'weight': []}
 
 for q in qs:
     rows = df[df['question_id'] == q]
     if len(rows) > 1:
-        besta = rows[rows['upvotebytime'] == max(rows['upvotebytime'])]
-        if besta['upvotebytime'].values[0] > 0:
-            bestv = besta['upvotebytime'].values[0]
+        besta = rows[rows['a_upvotebytime'] == max(rows['a_upvotebytime'])]
+        if besta['a_upvotebytime'].values[0] > 0:
             besta = besta['answerer'].values[0]
             
             for i, row in rows.iterrows():
                 if row['answerer'] != besta:
                     cben['from'].append(row['answerer'])
                     cben['to'].append(besta)
-                    
-                    vben['from'].append(row['answerer'])
-                    vben['to'].append(besta)
-                    vben['weight'].append(bestv - row['upvotebytime'])
-                    
-                    vben2['from'].append(row['answerer'])
-                    vben2['to'].append(besta)
-                    vben2['weight'].append(bestv - row['upvotebytime'])
-                    vben2['from'].append(row['asker'])
-                    vben2['to'].append(row['answerer'])
-                    vben2['weight'].append(bestv - row['upvotebytime'])
+
                     
 cben = pd.DataFrame(cben)
 cben.to_csv('../data/3-CBEN.tsv', index=False, sep = '\t')
 
+#VBEN, VBEN2
+qs = set(df['question_id'])
+vben = {'from': [], 'to':[], 'weight': []}
+vben2 = {'from': [], 'to':[], 'weight': []}
+
+for q in qs:
+    rows = df[df['question_id'] == q]
+    if len(rows) > 1:
+        for i, row in rows.iterrows():
+            bigger = rows[rows['a_upvotebytime'] > row['a_upvotebytime']]
+            for j, r in bigger.iterrows():
+                vben['from'].append(row['answerer'])
+                vben['to'].append(r['answerer'])
+                vben['weight'].append(r['a_upvotebytime'] - row['a_upvotebytime'])
+
+                vben2['from'].append(row['answerer'])
+                vben2['to'].append(r['answerer'])
+                vben2['weight'].append(r['a_upvotebytime'] - row['a_upvotebytime'])
+                
+            if row['a_upvotebytime'] > row['q_upvotebytime']:
+                vben2['from'].append(row['asker'])
+                vben2['to'].append(r['answerer'])
+                vben2['weight'].append(r['a_upvotebytime'] - row['q_upvotebytime'])
+    
 vben = pd.DataFrame(vben)
 vben.to_csv('../data/4-VBEN.tsv', index=False, sep = '\t')
 
